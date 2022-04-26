22d0c2c7b4250a259c57e975c00c808061389d71
Sarah Taylor <sarahtvylor@gmail.com>
2022-04-26 18:47:11 +0100

    Upload basic-mod1 challenge solution.

---
+++ basic-mod1.py
@@ -0,0 +1,21 @@
+def get_flag(flag_array):
+    flag = "".join(flag_array)
+    return "picoCTF{"+flag+"}"
+
+def decode_message(msg):
+    msg = msg.split(" ")
+    decoded_msg = []
+
+    for value in msg:
+        value = int(value) % 37
+        if value < 26:
+            value += 65
+            decoded_msg.append(chr(value))
+        elif value == 36:
+            decoded_msg.append("_")
+        else:
+            decoded_msg.append(str(value-26))
+    return decoded_msg
+
+
+print(get_flag(decode_message("202 137 390 235 114 369 198 110 350 396 390 383 225 258 38 291 75 324 401 142 288 397")))
